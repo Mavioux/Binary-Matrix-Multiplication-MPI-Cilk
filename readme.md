622989a030cd4a57ae48c29c12278e985e3b267b
mavioux <pakmarios@gmail.com>
2021-09-07 09:59:54 +0300

    Add matrices, work with csc

--- readme.md
+++ readme.md
@@ -2,3 +2,15 @@
 ../out/main_cilk ../matrices/test1.mtx ../matrices/test2.mtx ../matrices/test3.mtx
 mpirun -np 4 ../out/main_mpi ../matrices/test1.mtx ../matrices/test2.mtx ../matrices/test3.mtx
 mpirun -np 4 ../out/main_mpi_cilk ../matrices/test1.mtx ../matrices/test2.mtx ../matrices/test3.mtx
+belgium_osm.mtx
+com-Youtube.mtx
+dblp-2010.mtx
+mycielskian13.mtx
+NACA0015.mtx
+Oregon-1.mtx
+
+../out/main3 ../matrices/belgium_osm.mtx ../matrices/belgium_osm.mtx ../matrices/belgium_osm.mtx
+../out/main ../matrices/Oregon-1.mtx ../matrices/Oregon-1.mtx ../matrices/Oregon-1.mtx
+../out/main ../matrices/com-Youtube.mtx ../matrices/com-Youtube.mtx ../matrices/com-Youtube.mtx
+
+mpirun -np 4 ../out/main_mpi ../matrices/belgium_osm.mtx ../matrices/belgium_osm.mtx ../matrices/belgium_osm.mtx
--- src/main.c
+++ src/main.c
@@ -19,211 +19,219 @@
 #include <cilk/cilk_api.h>
 #include <time.h>
 
-int main(int argc, char** argv) {
+void readfile_stepone(
+    FILE *f,
+    uint32_t *m, 
+    uint32_t *n, 
+    uint32_t *nnz_pointer,
+    int *i, 
+    int *j,
+    int binary    
+    )
+    {
 
     int ret_code;
     MM_typecode matcode;
-    FILE *f1, *f2, *f3;
-    uint32_t I, J, K, nnz_a, nnz_b, nnz_f; 
-    int *I_A, *I_B, *I_F, *J_A, *J_B, *J_F;
-    double *val_a;
+    
+    if (mm_read_banner(f, &matcode) != 0)
+    {
+        printf("Could not process Matrix Market banner.\n");
+        exit(1);
+    }
 
-    clock_t begin;
-    clock_t end;
+
+    /*  This is how one can screen matrix types if their application */
+    /*  only supports a subset of the Matrix Market data types.      */
+
+    if (mm_is_complex(matcode) && mm_is_matrix(matcode) && 
+            mm_is_sparse(matcode) )
+    {
+        printf("Sorry, this application does not support ");
+        printf("Market Market type: [%s]\n", mm_typecode_to_str(matcode));
+        exit(1);
+    }
+
+    /* find out size of sparse matrix .... */
+
+    if ((ret_code = mm_read_mtx_crd_size(f, m, n, nnz_pointer)) !=0)
+        exit(1);
+}
+
+void readfile_steptwo(
+    FILE *f,
+    uint32_t *m, 
+    uint32_t *n, 
+    uint32_t *nnz_pointer,
+    int *I, 
+    int *J,
+    double * val,
+    int binary    
+    )
+    {
+
+    uint32_t M = *m;
+    uint32_t N = *n;
+    uint32_t nnz = *nnz_pointer;
+    
+    /* Depending on the second argument of the main call our original matrix may be binary or non binary so we read the file accordingly */
+    switch (binary) 
+    {
+    case 0:
+        /* use this if the source file is not binary */
+        for (uint32_t i=0; i<nnz; i++)
+        {
+            /* I is for the rows and J for the columns */
+            fscanf(f, "%d %d %lg\n", &I[i], &J[i], &val[i]);
+            I[i]--;  /* adjust from 1-based to 0-based */
+            J[i]--;
+        }
+        break;
+
+    case 1:
+        /* use this if the source file is binary */
+        for (uint32_t i=0; i<nnz; i++)
+        {
+            /* I is for the rows and J for the columns */
+            fscanf(f, "%d %d \n", &I[i], &J[i]);
+            I[i]--;  /* adjust from 1-based to 0-based */
+            J[i]--;
+        }
+        break;
+    
+    default:
+        printf("Not a valid second argument was passed\n");
+        exit(1);
+        break;
+    }
+
+    if (f !=stdin) fclose(f);
+
+    if(M != N) {
+        printf("COO matrix' columns and rows are not the same");
+    }
+}
+
+int main(int argc, char** argv) {
+
+    FILE *f;
+    uint32_t M, N, nnz;   
+    int *I, *J;
+    double *val;
+    int binary = 1;
+
+    struct timeval start, end;
 
     if (argc < 2)
 	{
-		fprintf(stderr, "Usage: %s [martix-market-filename-A] [martix-market-filename-B] [optional martix-market-filename-F]\n", argv[0]);
+		fprintf(stderr, "Usage: %s [martix-market-filename] [0 for non binary 1 for binary matrix]\n", argv[0]);
 		exit(1);
 	}
     else    
-    {
-        if ((f1 = fopen(argv[1], "r")) == NULL) 
-            exit(1);
-        if ((f2 = fopen(argv[2], "r")) == NULL) 
-            exit(1);
-        if ((f3 = fopen(argv[3], "r")) == NULL) 
+    { 
+        if ((f = fopen(argv[1], "r")) == NULL) 
             exit(1);
     }
 
-    /* reseve memory for matrices */
+    readfile_stepone(f, &M, &N, &nnz, I, J, binary);
+
     /* For the COO */
-    I_A = (int *) malloc(nnz_a * sizeof(int));
-    J_A = (int *) malloc(nnz_a * sizeof(int));
-    I_B = (int *) malloc(nnz_b * sizeof(int));
-    J_B = (int *) malloc(nnz_b * sizeof(int));
-    I_F = (int *) malloc(nnz_f * sizeof(int));
-    J_F = (int *) malloc(nnz_f * sizeof(int));
-    val_a = (double *) malloc(nnz_a * sizeof(double));
+    I = (uint32_t *) malloc(nnz * sizeof(uint32_t));
+    J = (uint32_t *) malloc(nnz * sizeof(uint32_t));
+    val = (double *) malloc(nnz * sizeof(double));
 
+    readfile_steptwo(f, &M, &N, &nnz, I, J, val, binary);
 
-    /* Read A matrix */
+    /* For the CSC */
+    uint32_t* a_cscRow = (uint32_t *) malloc(nnz * sizeof(uint32_t));
+    uint32_t* a_cscColumn = (uint32_t *) malloc((N + 1) * sizeof(uint32_t));
+    /* For the CSC */
+    uint32_t* b_csrColumn = (uint32_t *) malloc(nnz * sizeof(uint32_t));
+    uint32_t* b_csrRow = (uint32_t *) malloc((M + 1) * sizeof(uint32_t));
 
-    if (mm_read_banner(f1, &matcode) != 0)
-    {
-        printf("Could not process Matrix Market banner.\n");
-        exit(1);
+    /* Initialize c3 with zeros*/
+    int* c3;
+    c3 = malloc(N * sizeof c3);    
+    for(int i = 0; i < N; i++){
+        c3[i] = 0;
     }
 
-    /*  This is how one can screen matrix types if their application */
-    /*  only supports a subset of the Matrix Market data types.      */
+    coo2csc(a_cscRow, a_cscColumn, I, J, nnz, M, 0);
+    coo2csc(b_csrColumn, b_csrRow, J, I, nnz, N, 0);
 
-    if (mm_is_complex(matcode) && mm_is_matrix(matcode) && 
-            mm_is_sparse(matcode) )
-    {
-        printf("Sorry, this application does not support ");
-        printf("Market Market type: [%s]\n", mm_typecode_to_str(matcode));
-        exit(1);
-    }
+    /* For the C CSC */
+    uint32_t* c_cscRow = (uint32_t *) malloc(0 * sizeof(uint32_t));
+    uint32_t* c_values = (uint32_t *) malloc(0 * sizeof(uint32_t));
+    uint32_t* c_cscColumn = (uint32_t *) malloc((N + 1) * sizeof(uint32_t));
 
-    /* find out size of sparse matrix .... */
+    c_cscColumn[0] = 0;
+    c_cscRow = realloc(c_cscRow, 2 * nnz * sizeof(int));
+    c_values = realloc(c_values, 2 * nnz * sizeof(int));
 
-    if ((ret_code = mm_read_mtx_crd_size(f1, &I, &K, &nnz_a)) !=0)
-        exit(1);
-    
-    printf("I: %d, K: %d, nnz_a: %d\n", I, K, nnz_a);
-
-    uint8_t** a = (uint8_t**) malloc(I * sizeof(uint8_t*));
-    for(int i = 0; i < I; i++) {
-        a[i] = (uint8_t*) malloc(K * sizeof(uint8_t));
-        memset(a[i], 0, I * sizeof(uint8_t));    
-    }
-
-    /* Read from the file */
-    for (uint32_t i=0; i < nnz_a; i++) {
-        /* I is for the rows and J for the columns */
-        fscanf(f1, "%d %d\n", &I_A[i], &J_A[i]);
-        // printf("I_A[%d] = %d\n", i, I_A[i]);
-        // printf("J_A[%d] = %d\n", i, J_A[i]);
-        I_A[i]--;  /* adjust from 1-based to 0-based */
-        J_A[i]--;
-
-        a[I_A[i]][J_A[i]] = 1;
-    }
-
-    if (f1 !=stdin) fclose(f1);
-    
-    
-
-    /* Read B matrix */
-
-    if (mm_read_banner(f2, &matcode) != 0)
-    {
-        printf("Could not process Matrix Market banner.\n");
-        exit(1);
-    }
-
-    /*  This is how one can screen matrix types if their application */
-    /*  only supports a subset of the Matrix Market data types.      */
-
-    if (mm_is_complex(matcode) && mm_is_matrix(matcode) && 
-            mm_is_sparse(matcode) )
-    {
-        printf("Sorry, this application does not support ");
-        printf("Market Market type: [%s]\n", mm_typecode_to_str(matcode));
-        exit(1);
-    }
-
-    /* find out size of sparse matrix .... */
-
-    if ((ret_code = mm_read_mtx_crd_size(f2, &K, &J, &nnz_b)) !=0)
-        exit(1);
-    
-    printf("K: %d, J: %d, nnz_b: %d\n", K, J, nnz_b);
-
-   uint8_t** b = (uint8_t**) malloc(K * sizeof(uint8_t*));
-    for(int i = 0; i < K; i++) {
-        b[i] = (uint8_t*) malloc(J * sizeof(uint8_t));
-        memset(b[i], 0, K * sizeof(uint8_t));    
-    }
-
-    /* Read from the file */
-    for (uint32_t i=0; i < nnz_b; i++) {
-        /* I is for the rows and J for the columns */
-        fscanf(f2, "%d %d\n", &I_B[i], &J_B[i]);
-        I_B[i]--;  /* adjust from 1-based to 0-based */
-        J_B[i]--;
-        b[I_B[i]][J_B[i]] = 1;
-    }
-
-    if (f2 !=stdin) fclose(f2);
-    
-    
-    // /* Read F matrix */
-
-    // if (mm_read_banner(f3, &matcode) != 0)
-    // {
-    //     printf("hello\n");
-    //     printf("Could not process Matrix Market banner.\n");
-    //     exit(1);
-    // }
-
-    // /*  This is how one can screen matrix types if their application */
-    // /*  only supports a subset of the Matrix Market data types.      */
-
-    // if (mm_is_complex(matcode) && mm_is_matrix(matcode) && 
-    //         mm_is_sparse(matcode) )
-    // {
-    //     printf("Sorry, this application does not support ");
-    //     printf("Market Market type: [%s]\n", mm_typecode_to_str(matcode));
-    //     exit(1);
-    // }
-    
-    // /* find out size of sparse matrix .... */
-
-    // if ((ret_code = mm_read_mtx_crd_size(f3, &I, &J, &nnz_f)) !=0)
-    //     exit(1);
-    
-    // printf("I: %d, J: %d, nnz_f: %d", I, J, nnz_f);
-
-    // /* Read from the file */
-    // for (uint32_t i=0; i < nnz_f; i++) {
-    //     /* I is for the rows and J for the columns */
-    //     fscanf(f3, "%d %d\n", &I_F[i], &J_F[i]);
-    //     printf("I_F[%d] = %d\n", i, I_F[i]);
-    //     printf("J_F[%d] = %d\n", i, J_F[i]);
-    //     I_F[i]--;  /* adjust from 1-based to 0-based */
-    //     J_F[i]--;
-    // }
-
-    // if (f3 !=stdin) fclose(f3);
-
-    /* Allocate memory for the result of the multiplication */
-    uint8_t** res = (uint8_t**) malloc(I * sizeof(uint8_t*));
-    for(int i = 0; i < I; i++) {
-        res[i] = (uint8_t*) malloc(J * sizeof(uint8_t));
-        memset(res[i], 0, J * sizeof(uint8_t));
-    }
-    
-    // /* Algorithm starts here */
+    printf("Matrix Loaded, now Searching!\n");    
 
     // Start measuring time
-    begin = clock();
+    gettimeofday(&start,NULL); 
 
-    for(int i = 0; i < I; i++) {
-        for(int j = 0; j < J; j++) {
-            for(int k = 0; k < K; k++) {
-                if(a[i][k] * b[k][j]) {
-                    res[i][j] = 1;
-                    goto next_iteration;
-                }
+    /* ALGORITHM STARTS HERE */
+
+    // C = A.*(A*A)
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < a_cscColumn[i+1] - a_cscColumn[i]; j++) {
+            int a_row = a_cscRow[a_cscColumn[i] + j];
+            int a_col = i;
+
+            // Element of (A*A)[i,j]
+            int k_size = a_cscColumn[a_row+1] - a_cscColumn[a_row];  
+            int l_size = b_csrRow[a_col+1] - b_csrRow[a_col];    
+            int *l = malloc((l_size) * sizeof(int));
+            int *k = malloc((k_size) * sizeof(int));
+            /* Create the l vector with the appropriate values */
+            for(int x = 0; x < k_size; x++) {
+                k[x] = a_cscRow[a_cscColumn[a_row] + x];
             }
-        }
-        next_iteration: ;
-    }
+            for(int x = 0; x < l_size; x++) {
+                l[x] = b_csrColumn[b_csrRow[a_col] + x];
+            }
 
-    end = clock();
-    double duration = (double)(end - begin) / CLOCKS_PER_SEC;
+            int k_pointer = 0;
+            int l_pointer = 0;
+            int value = 0;
+
+            while(k_pointer != k_size && l_pointer != l_size) {
+                if(k[k_pointer] == l[l_pointer]) {
+                    value = 1;
+                    break;
+                }
+                else if(k[k_pointer] > l[l_pointer]) {
+                    l_pointer++;
+                }
+                else
+                {
+                    k_pointer++;
+                }                
+            }        
+
+            if(value) {
+                c_values[a_cscColumn[i] + j] = 1;
+            }
+            else {
+                c_values[a_cscColumn[i] + j] = 0;
+            }
+            free(l);
+            free(k);
+        }
+    }
+    c_cscColumn = a_cscColumn;
+    c_cscRow = a_cscRow;
+
+    gettimeofday(&end,NULL);
+    double duration = (end.tv_sec+(double)end.tv_usec/1000000) - (start.tv_sec+(double)start.tv_usec/1000000);
     printf("Duration: %f\n", duration);
 
-    // /* Print result */
-    // printf("result matrix\n");
-    // for(int i = 0; i < I; i++) {
-    //     printf("\n");
-    //     for(int j = 0; j < J; j++) {
-    //         printf("%u\t", res[i][j]);
-    //     }
-    // }
+    /* Deallocate the arrays */
+    free(I);
+    free(J);
+    free(c3);
 
+    return 0;
 }
-
